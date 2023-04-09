21205bb6aafe2ae0cbbc8e27b84c73a5fcbf259a
haluu102 <ha.luu102@hcmut.edu.vn>
2023-04-10 00:50:28 +0700

    format

--- server/database/insertClass.sql
+++
@@ -1,3 +0,0 @@
-    INSERT INTO `class` VALUES('2023-01-30','2023-04-30','TOEIC1','active','40','30');
-    INSERT INTO `class` VALUES('2023-01-30','2023-04-30','TOEIC2','active','40','40');
-    INSERT INTO `class` VALUES('2023-01-30','2023-04-30','TOEIC3','disactive','40','20');
