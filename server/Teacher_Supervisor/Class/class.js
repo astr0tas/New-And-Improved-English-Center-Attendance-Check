ef11b93e08a0e272fd94e1ba488002c170fe0dee
nghia.duong272919136 <nghia.duong272919136@hcmut.edu.vn>
2023-04-28 23:35:26 +0700

    updates

--- server/Teacher_Supervisor/Class/class.js
+++ server/Teacher_Supervisor/Class/class.js
@@ -8,7 +8,7 @@ MyClassAPI.get('/myClasses', (req, res) =>
       getClassList(req.query.id, req.query.offset, (err, result) =>
       {
             if (err)
-                  res.status(500).send('Error retrieving user from database.');
+                  res.status(500).send('Error retrieving data from database.');
             else
             {
                   res.status(200).send(result);
@@ -21,7 +21,7 @@ MyClassAPI.get('/myClasses/getCurrentStudent', (req, res) =>
       getCurrentStudent(req.query.className, (err, result) =>
       {
             if (err)
-                  res.status(500).send('Error retrieving user from database.');
+                  res.status(500).send('Error retrieving data from database.');
             else
             {
                   res.status(200).send(result[0]);
@@ -34,7 +34,7 @@ MyClassAPI.get('/myClasses/detail', (req, res) =>
       getClassDetail(req.query.className, (err, result) =>
       {
             if (err)
-                  res.status(500).send('Error retrieving user from database.');
+                  res.status(500).send('Error retrieving data from database.');
             else
             {
                   res.status(200).send(result[0]);
@@ -47,7 +47,7 @@ MyClassAPI.get('/myClasses/getSessions', (req, res) =>
       getSessions(req.query.className, (err, result) =>
       {
             if (err)
-                  res.status(500).send('Error retrieving user from database.');
+                  res.status(500).send('Error retrieving data from database.');
             else
             {
                   res.status(200).send(result[0]);
@@ -60,7 +60,7 @@ MyClassAPI.get('/myClasses/getStudentList', (req, res) =>
       getStudentList(req.query.className, (err, result) =>
       {
             if (err)
-                  res.status(500).send('Error retrieving user from database.');
+                  res.status(500).send('Error retrieving data from database.');
             else
             {
                   res.status(200).send(result);
@@ -73,7 +73,7 @@ MyClassAPI.get('/myClasses/getSessionList', (req, res) =>
       getSessionList(req.query.className, (err, result) =>
       {
             if (err)
-                  res.status(500).send('Error retrieving user from database.');
+                  res.status(500).send('Error retrieving data from database.');
             else
             {
                   res.status(200).send(result);
